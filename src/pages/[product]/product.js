47be716cf853dae15ef05e57f82f4db87dc34400
JefferDevop <somadesarrollo@gmail.com>
2023-09-14 08:32:41 -0500

    prueba 1

--- src/pages/[product]/product.js
+++ src/pages/[product]/product.js
@@ -3,14 +3,13 @@ import { DetailProduct, FooterApp, Redes, Separator } from "@/components";
 import { Footer } from "@/components";
 
 export default function ProductPage(props) {
-  const { product, relate, gallery } = props;
-
+  const { product, relate } = props;
 
   return (
     <div>
       <BasicLayout>
         <Redes />
-        <DetailProduct product={product} relate={relate} gallery= {gallery} />
+        <DetailProduct product={product} relate={relate} />
         <FooterApp />
         <Footer />
       </BasicLayout>
