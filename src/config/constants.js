5a881b640929ed9e21624782e8ef7e61368176b4
JefferDevop <somadesarrollo@gmail.com>
2023-11-22 08:55:04 -0500

    buscar por varios campos ref, slug etc

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,8 +1,8 @@
 
-export const BASE_API = NEXT_PUBLIC_API 
+export const BASE_API = "https://lekar.catalogointeractivo.com.co"
 //export const BASE_API = `https://planos.centronube.com`;
 
 
 export const SEARCH_HISTORY = "search-history";
 export const CART = "cart";
-export const BASE_NAME = NEXT_PUBLIC_STORAGE 
+export const BASE_NAME = "https://res.cloudinary.com/dnio5vufj/"
