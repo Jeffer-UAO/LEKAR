16d55ad84d12e2cd0eb6022e08fa7ca4fa098be8
JefferDevop <somadesarrollo@gmail.com>
2023-12-19 13:37:32 -0500

    Variables locales .env

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,6 +1,8 @@
 
-export const BASE_API = process.env.API
-export const BASE_NAME = process.env.STORAGE
+export const BASE_API = "https://lekar.catalogointeractivo.com.co"
+export const BASE_NAME = "https://res.cloudinary.com/dnio5vufj/"
+//export const BASE_API = `https://planos.centronube.com`;
+
 
 export const SEARCH_HISTORY = "search-history";
 export const CART = "cart";
